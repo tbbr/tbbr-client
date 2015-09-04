99dcee6f06fee165b463cd3174d9a269c23f7e10
MaazAli <maaz_ali@outlook.com>
2015-09-04 00:16:54 -0700

    Add default serializer and group model

--- app/models/group.js
+++ app/models/group.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  hashId: DS.attr('string'),
+  createdAt: DS.attr('date'),
+  updatedAt: DS.attr('date')
 });
---
+++ app/serializers/application.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+  keyForAttribute: function(attr, method) {
+    return Ember.String.underscore(attr);
+  }
+});
