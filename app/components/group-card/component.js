f21635482771b694fddad7cb4d13898d695ba677
MaazAli <maaz_ali@outlook.com>
2015-09-27 22:36:39 -0700

    Implement group-card component

--- app/components/group-card/component.js
+++ app/components/group-card/component.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  users: [
+    {
+      name: "Maaz"
+    },
+    {
+      name: "Rabia"
+    },
+    {
+      name: "Faraz"
+    }
+  ]
 });
