c6ccae9376cb0478f04bddbf176868e7cf97b868
MaazAli <maaz_ali@outlook.com>
2015-10-20 22:49:29 -0700

    Ensure Authorization header is now sent with ember-data requests

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import config from '../config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: config.apiHost,
-  // authorizer: 'authorizer:application'
+  authorizer: 'authorizer:oauth2'
 });
