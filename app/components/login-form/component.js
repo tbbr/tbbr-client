6e9425a39751adee5f3c3fb690afdaec109f7352
MaazAli <maaz_ali@outlook.com>
2015-10-26 20:13:06 -0700

    Ensure users are redirected after they login

--- app/components/login-form/component.js
+++ app/components/login-form/component.js
@@ -6,7 +6,9 @@ export default Ember.Component.extend({
   session: service('session'),
   actions: {
     loginFacebook: function() {
-      this.get('session').authenticate('authenticator:torii', 'facebook')
+      this.get('session').authenticate('authenticator:torii', 'facebook').then(() => {
+        this.sendAction('onLoginSuccess')
+      })
     }
   }
 })
