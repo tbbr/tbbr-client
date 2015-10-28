f5410979695e699e57f2399588c6c8821bf97ada
MaazAli <maaz_ali@outlook.com>
2015-10-27 22:44:09 -0700

    Add random colors to group images for some flavour

--- app/components/group-card/component.js
+++ app/components/group-card/component.js
@@ -1,7 +1,21 @@
 import Ember from 'ember'
 
+let LetterToColor = [
+  '#dd7788',
+  '#667799',
+  '#7a9460',
+  '#dd9977',
+  '#665566',
+  '#223333'
+]
+
 export default Ember.Component.extend({
   groupFirstLetter: function() {
     return this.get('group.name')[0]
-  }.property('group.name')
+  }.property('group.name'),
+
+  groupLetterColor: function() {
+    let random = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
+    return LetterToColor[random]
+  }.property('groupFirstLetter')
 });
