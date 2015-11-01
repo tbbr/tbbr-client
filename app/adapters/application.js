550bc12a4c79424012a8d661f2b943b5405f1e49
MaazAli <maaz_ali@outlook.com>
2015-11-01 15:43:04 -0800

    Add api as namespace for the time being

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,5 +4,6 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: config.apiHost,
+  namespace: 'api',
   authorizer: 'authorizer:oauth2'
 });
