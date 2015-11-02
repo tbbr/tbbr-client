3a9ec440eca86b9fdbecda72a23bfde104c8fa96
MaazAli <maaz_ali@outlook.com>
2015-11-01 17:06:57 -0800

    Change app name

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'payup-client',
+    modulePrefix: 'tbbr-client',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
