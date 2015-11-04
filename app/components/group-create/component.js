fa714b80c4c4d969f5c9800abfd09596ba3f7a65
MaazAli <maaz_ali@outlook.com>
2015-11-04 00:11:54 -0800

    Ensure group name exists before creating a group

--- app/components/group-create/component.js
+++ app/components/group-create/component.js
@@ -8,13 +8,21 @@ export default Ember.Component.extend({
   description: null,
   actions: {
     cancelCreateAction: function() {
-      this.sendAction('cancelCreateAction');
+      this.sendAction('cancelCreateAction')
     },
     createGroup: function() {
+      let name = this.get('name')
+      let description = this.get('description')
+
+      if (!name) {
+        return
+      }
+
       let group = this.get('store').createRecord('group', {
         name: this.get('name'),
         description: this.get('description')
-      });
+      })
+
       group.save().then(() => {
         this.sendAction('cancelCreateAction')
       })
