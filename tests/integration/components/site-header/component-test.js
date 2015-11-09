dfc6675aeec9c78e0584140aa25140ea5a0d5376
MaazAli <maaz_ali@outlook.com>
2015-11-08 23:05:58 -0800

    Add asserts for testing current user info of site-header component

--- tests/integration/components/site-header/component-test.js
+++ tests/integration/components/site-header/component-test.js
@@ -2,19 +2,31 @@ import { moduleForComponent, test } from 'ember-qunit'
 import { skip } from 'qunit'
 import hbs from 'htmlbars-inline-precompile'
 
+const sessionUserStub = Ember.Service.extend({
+  isAuthenticated: true,
+  current: Ember.Object.create({
+    name: 'Test User',
+    avatarUrl: 'http://testimage.com/image.jpg'
+  })
+})
+
+
 moduleForComponent('site-header', 'Integration | Component | site header', {
-  integration: true
+  integration: true,
+
+  beforeEach: function () {
+    this.register('service:session-user', sessionUserStub)
+    this.inject.service('session-user', { as: 'sessionUser' })
+  }
 })
 
 test('it renders correct content', function(assert) {
-  assert.expect(1)
+  assert.expect(3)
 
   this.render(hbs`{{site-header}}`)
 
   assert.equal(this.$('.logo').text().trim(), 'tbbr', 'renders the site name in correct position')
-})
+  assert.equal(this.$('.user-info span').text().trim(), 'Test User', 'renders the name of the user currently logged in')
 
-skip('it renders user related information', function(assert) {
-  // Test user related information is rendered, when we can figure out
-  // how to stub out services
+  assert.equal(this.$('.user-info .avatar').css('background-image'), 'url(http://testimage.com/image.jpg)', 'renders the avatar of the user currently logged in')
 })
