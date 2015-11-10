dd06a6616abe57a6b77549401621b7560bea487d
MaazAli <maaz_ali@outlook.com>
2015-11-10 00:08:13 -0800

    Collapse sidebar when window width is small

--- app/components/side-bar/component.js
+++ app/components/side-bar/component.js
@@ -1,10 +1,32 @@
 import Ember from 'ember'
 
 export default Ember.Component.extend({
+  classNameBindings: ['isToggleable:toggleable', 'isClosed:closed'],
+
+  didInsertElement() {
+    if (Ember.$(window).width() < 900) {
+      // Allow toggling of sidebar
+      this.set('isToggleable', true)
+      this.set('isClosed', true)
+    }
+
+    Ember.$(window).on('resize', () => {
+      if (Ember.$(window).width() < 900) {
+        this.set('isToggleable', true)
+        this.set('isClosed', true)
+      } else if (Ember.$(window).width() > 900) {
+        this.set('isToggleable', false)
+        this.set('isClosed', false)
+      }
+    })
+  },
   isCreatingGroup: false,
   actions: {
     toggleCreatingGroup: function() {
       this.toggleProperty('isCreatingGroup')
+    },
+    toggleSidebar: function() {
+      this.toggleProperty('isClosed')
     }
   }
 })
