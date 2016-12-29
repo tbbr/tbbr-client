812d59b6c252c5cb09e7cc8a7abcaceb293731c5
MaazAli <maaz_ali@outlook.com>
2016-12-28 23:13:39 -0500

    Update bucket for index.html file

--- config/deploy.js
+++ config/deploy.js
@@ -11,7 +11,7 @@ module.exports = function(deployTarget) {
         's3-index': {
             accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
             secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-            bucket: 'tbbrassets',
+            bucket: 'tbbr.me',
             region: 'us-east-1'      
         }
     }
